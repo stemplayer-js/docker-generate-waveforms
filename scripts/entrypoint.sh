dd2d858c1422e4a0be086091fb72ed0ce7fd5c12
Mark Cremer <mark@firstcoders.co.uk>
2023-10-05 09:31:53 +0100

    chore: added pixels-per-second flag

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,6 +3,7 @@
 # non_interactive=0
 SEGMENT_TIME=10
 BITRATE=128
+PIXELS_PER_SECOND=50
 
 ################################################################################
 # Help                                                                         #
@@ -12,36 +13,40 @@ Help()
    # Display Help
    echo "Generate segments of mp3s from source wav files."
    echo
-   echo "Syntax: scriptTemplate [-help|-input|-output|--segment_time|--bitrate]"
+   echo "Syntax: scriptTemplate [-help|-input|-output|--segment_time|--bitrate|--pixels-per-second]"
    echo "options:"
-   echo "--help         Print this Help."
-   echo "--input        The input file."
-   echo "--output       The output folder."
-   echo "--segment_time The duration of each segment (default 10)"
-   echo "--bitrate      Bitrate of each encoded mp3 segment (default 128)"
+   echo "--help              Print this Help."
+   echo "--input             The input file."
+   echo "--output            The output folder."
+   echo "--segment_time      The duration of each segment (default 10)"
+   echo "--pixels-per-second The pixels per second (default 100)"
+   echo "--bitrate           Bitrate of each encoded mp3 segment (default 128)"
 }
 
 while [ "$1" != "" ]; do
     case $1 in
-        --help )            shift
-                            Help
-                            exit
-                            ;;
-        --input )           shift
-                            INPUT_FILE=$1
-                            ;;
-        --output )          shift
-                            OUTPUT_FOLDER=$1
-                            ;;
-        --segment_time )    shift
-                            SEGMENT_TIME=$1
-                            ;;
-        --bitrate )         shift
-                            BITRATE=$1
-                            ;;
-        * )                 echo "Invalid parameters."
-                            Help
-                            exit 1
+        --help )                  shift
+                                  Help
+                                  exit
+                                  ;;
+        --input )                 shift
+                                  INPUT_FILE=$1
+                                  ;;
+        --output )                shift
+                                  OUTPUT_FOLDER=$1
+                                  ;;
+        --segment_time )          shift
+                                  SEGMENT_TIME=$1
+                                  ;;
+        --bitrate )               shift
+                                  BITRATE=$1
+                                  ;;
+        --pixels-per-second )     shift
+                                  PIXELS_PER_SECOND=$1
+                                  ;;
+        * )                       echo "Invalid parameters."
+                                  Help
+                                  exit 1
     esac
     shift
 done
@@ -69,7 +74,7 @@ file="$(echo ${s%.*})"
 
 outfile="$OUTPUT_FOLDER/$file.json"
 
-audiowaveform -i "$INPUT_FILE" -o "$outfile" --pixels-per-second 20 --bits 16
+audiowaveform -i "$INPUT_FILE" -o "$outfile" --pixels-per-second "$PIXELS_PER_SECOND" --bits 16
 
 echo "Normalising waveform data"
 (python /scripts/normalise.py "$outfile")
